56a8fc9338dd081e98fe5752066edcc60145f2c7
Antonio-Neves <antoniocalheirosneves@gmail.com>
2025-07-30 10:40:19 +0100

    changed post to create new user

--- fast_zero/fast_zero/app.py
+++ fast_zero/fast_zero/app.py
@@ -1,9 +1,49 @@
 from fastapi import FastAPI
+from http import HTTPStatus
+
+from fast_zero.schemas import Message, UserSchema, UserPublic, UserDB
 
 
-app = FastAPI()
+app = FastAPI(title='EM FastApi - Curso')
 
-@app.get('/')
+database = [
+#     {
+#       "username": "Deus",
+#       "email": "Deus@deus.com",
+#       "password": "Obrigado",
+#       "id": 1
+#     },
+#     {
+#       "username": "Rui",
+#       "email": "rui@rui.com",
+#       "password": "ruimano",
+#       "id": 2
+#     },
+#     {
+#       "username": "Duarte",
+#       "email": "duarte@duarte.com",
+#       "password": "duartemano",
+#       "id": 3
+#     }
+]
+
+
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Hello World!'}
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+
+    user_with_id = UserDB(
+        username=user.username,
+        email=user.email,
+        password=user.password,
+        id=len(database) + 1
+    )
+
+    database.append(user_with_id)
+
+    return user_with_id
 

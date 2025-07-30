80264ddd2ae26dc6b28409a5438b94c4866d7274
Antonio-Neves <antoniocalheirosneves@gmail.com>
2025-07-30 10:40:32 +0100

    created tests

--- fast_zero/tests/test_app.py
+++ fast_zero/tests/test_app.py
@@ -1,4 +1,14 @@
 from fastapi.testclient import TestClient
+from http import HTTPStatus
 
 from fast_zero.app import app
 
+client = TestClient(app)
+
+
+def test_root_deve_retornar_hello_world():
+
+    response = client.get('/')
+
+    assert response.json() == {'message': 'Hello World!'}
+    assert response.status_code == HTTPStatus.OK

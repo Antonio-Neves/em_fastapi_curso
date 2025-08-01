a2f95b0e1d1bc7b1f56ff4e91434c3eed30b31f2
Antonio-Neves <antoniocalheirosneves@gmail.com>
2025-08-01 09:11:48 +0100

    Changed post function

--- fast_zero/fast_zero/app.py
+++ fast_zero/fast_zero/app.py
@@ -7,24 +7,24 @@ from fast_zero.schemas import Message, UserSchema, UserPublic, UserDB
 app = FastAPI(title='EM FastApi - Curso')
 
 database = [
-#     {
-#       "username": "Deus",
-#       "email": "Deus@deus.com",
-#       "password": "Obrigado",
-#       "id": 1
-#     },
-#     {
-#       "username": "Rui",
-#       "email": "rui@rui.com",
-#       "password": "ruimano",
-#       "id": 2
-#     },
-#     {
-#       "username": "Duarte",
-#       "email": "duarte@duarte.com",
-#       "password": "duartemano",
-#       "id": 3
-#     }
+    # {
+    #   "username": "Deus",
+    #   "email": "Deus@Deus.com",
+    #   "password": "Obrigado",
+    #   "id": 1
+    # },
+    # {
+    #   "username": "Rui",
+    #   "email": "rui@rui.com",
+    #   "password": "ruimano",
+    #   "id": 2
+    # },
+    # {
+    #   "username": "Duarte",
+    #   "email": "duarte@duarte.com",
+    #   "password": "duartemano",
+    #   "id": 3
+    # }
 ]
 
 
@@ -37,9 +37,10 @@ def read_root():
 def create_user(user: UserSchema):
 
     user_with_id = UserDB(
-        username=user.username,
-        email=user.email,
-        password=user.password,
+        # username=user.username,
+        # email=user.email,
+        # password=user.password,
+        **user.model_dump(), # Unpack and transform again in a dict with model_dump
         id=len(database) + 1
     )
 

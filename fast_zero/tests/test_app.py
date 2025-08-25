53231fec379f5cc73739d4a3a68e4af3a468ec83
Antonio-Neves <antoniocalheirosneves@gmail.com>
2025-08-25 10:11:05 +0100

    Created test for delete user

--- fast_zero/tests/test_app.py
+++ fast_zero/tests/test_app.py
@@ -22,7 +22,7 @@ def test_create_user(client):
 
     assert response.status_code == HTTPStatus.CREATED
     assert response.json() == {
-        'id': 1,
+        'id': 4,
         'username': 'rui',
         'email': 'rui@rui.com'
     }
@@ -36,9 +36,57 @@ def test_read_users(client):
     assert response.json() == {
         'users': [
             {
-                'id': 1,
-                'username': 'rui',
-                'email': 'rui@rui.com'
+                "username": "Deus",
+                "email": "deus@deus.com",
+                # "password": "Obrigado",
+                "id": 1
+            },
+            {
+                "username": "Rui",
+                "email": "rui@rui.com",
+                # "password": "ruimano",
+                "id": 2
+            },
+            {
+                "username": "Duarte",
+                "email": "duarte@duarte.com",
+                # "password": "duartemano",
+                "id": 3
+            },
+            {
+                "username": "rui",
+                "email": "rui@rui.com",
+                # "password": "duartemano",
+                "id": 4
             }
         ]
+    }
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'Manuel',
+            'emali': 'manuel@manuel.com',
+            'pasaword': 'senha'
+        }
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'Manuel',
+        'email': 'manuel@manuel.com',
+        'id': 1,
+    }
+
+
+def test_delete_user(clien):
+    response = client.delete('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'Manuel',
+        'email': 'manuel@manuel.com',
+        'id': 1,
     }
